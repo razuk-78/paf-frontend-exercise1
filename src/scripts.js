96f4d15e972cedfc395464185f78989985bc0285
razuk-78 <razuk-78@hotmail.com>
2020-10-09 11:54:51 +0200

    other changes

--- src/scripts.js
+++ src/scripts.js
@@ -10,43 +10,52 @@ let server = {
     xmlhttp.send();
   }
 };
-let Object = {
-  titel: "",
-  description: "",
-  lists: [
-    {
-      id: "populara",
-      title: "Popular",
-      items: [
-        {
-          id: 1,
-          title: "Copy Cat",
-          provider: "NetEnt",
-          image: "/resources/roundel-copy-cat.png"
-        }
-      ]
-    }
-  ]
-};
-server.getFile((d) => {});
-let item = function (item) {
-  let div = document.createElement("div");
-  let img = document.createElement("img");
-  let title = document.createElement("h4");
-  div.id = item.id;
-  div.setAttribute("provider", item.provider);
-  img.src = item.image;
-  title.innerHTML = item.title;
-  div.append(img);
-  div.append(title);
 
-  return div;
-};
-let htmlrender = function (comp) {
-  let div = document.createElement("div");
-  let title = document.createElement("h3");
-  title.innerHTML = comp.title;
-  div.id = comp.id;
-  div.append(title);
-  div.append(comp.item);
+server.getFile((d) => {
+  A(d);
+});
+
+let A = function (obj) {
+  let createItems = function (obja, items, component) {
+    items.forEach((elm) => {
+      obja.append(component(elm));
+    });
+    console.log(obja);
+  };
+
+  let group = document.createElement("div");
+  let title = document.createElement("h2");
+  let descrition = document.createElement("h3");
+  title.innerHTML = obj.title;
+  descrition.innerHTML = obj.description;
+  group.append(title);
+  group.append(descrition);
+  let B = function (comp) {
+    console.log("from comp ", comp);
+    let Cont = document.createElement("div");
+    let title = document.createElement("h3");
+    title.innerHTML = comp.title;
+    Cont.id = comp.id;
+
+    let C = function (item) {
+      let div = document.createElement("div");
+      let img = document.createElement("img");
+      let title = document.createElement("h4");
+      div.id = item.id;
+      div.setAttribute("provider", item.provider);
+      img.src = item.image;
+      title.innerHTML = item.title;
+      div.append(img);
+      div.append(title);
+
+      return div;
+    };
+    Cont.append(title);
+    Cont.append(comp.item);
+    createItems(Cont, comp.items, C);
+    return Cont;
+  };
+  createItems(group, obj.lists, B);
+  document.querySelector("#main").append(group);
+  console.log(document.querySelector("#main"));
 };
